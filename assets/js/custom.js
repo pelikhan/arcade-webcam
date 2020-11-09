dd96732c361e841b393f51b6b314c7463c4724ef
Peli de Halleux <peli@Pelis-MacBook-Pro.local>
2020-11-09 21:03:50 +0100

    some work on mapping image

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,20 @@
-/**
- * This will be loaded before starting the simulator.
- * If you wish to add custom javascript, 
- * ** make sure to add this line to pxt.json**
- * 
- *      "disableTargetTemplateFiles": true
- * 
- * otherwise MakeCode will override your changes.
- * 
- * To register a constrol simmessages, use addSimMessageHandler
- */
+document.addEventListener("DOMContentLoaded", function () {
+    const video = document.createElement("video");
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    canvas.width = 120
+    canvas.height = 160
+
+    navigator.getUserMedia({ video: true },
+        () => video.src = window.URL.createObjectURL(stream),
+        e => { });
+
+    function draw() {
+        if (video.paused || video.ended) return false;
+        context.drawImage(v, 0, 0, w, h);
+        // convert canvas to pxt iamge
+    }
+
+    // draw at 50fps
+    setInterval(draw, 20);
+})
